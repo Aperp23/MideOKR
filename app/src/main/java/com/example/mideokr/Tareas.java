13890d19b2d6cc3838fa038386e59ed3b43590a8
Aperp23 <a.perez.pruebas@gmail.com>
2023-05-05 09:59:18 +0200

    modificación

---
+++ app/src/main/java/com/example/mideokr/Tareas.java
@@ -0,0 +1,14 @@
+package com.example.mideokr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tareas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas);
+    }
+}
